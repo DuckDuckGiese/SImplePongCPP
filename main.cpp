9173a99ea98c16ff7a5cdfe9c65794d0a3c405dd
Ricky Giese <richard.giese@icloud.com>
2023-10-28 11:23:51 -0500

    Part 2

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <time.h>
+#include <conio.h>
 using namespace std;
 enum eDir { STOP = 0, LEFT = 1, UPLEFT = 2, DOWNLEFT = 3, RIGHT = 4, UPRIGHT = 5, DOWNRIGHT = 6};
 
@@ -87,21 +89,54 @@ public:
     inline int getY() { return y; }
     inline void moveUp() { y--; }
     inline void moveDown() { y++; }
+    friend ostream & operator<<(ostream & o, cPaddle c)
+    {
+        o << "Paddle [" << c.x << ", " << c.y << "]" << endl;
+        return o;
+    }
+};
+class cGameManager
+{
+private:
+    int width, height;
+    int score1, score2;
+    char up1, down1, up2, down2;
+    bool quit;
+    cBall * ball;
+    cPaddle *player1;
+    cPaddle *player2;
+public:
+    cGameManager(int w, int h)
+    {
+        srand(time(NULL));
+        quit = false;
+        up1 = 'w'; up2 = 'i';
+        down1 = 's'; down2 = 'k';
+        score1 = score2 = 0;
+        width = w; height = h;
+        ball = new cBall(w / 2, h / 2);
+        player1 = new cPaddle(1, h/2 - 3);
+        player2 = new cPaddle(w - 2, h / 2 - 3);
+    }
+    ~cGameManager()
+    {
+        delete ball, player1, player2;
+    }
+    void ScoreUp(cPaddle * player)
+    {
+        if (player == player1)
+            score1++;
+        else if (player == player2)
+            score2++;
+
+        ball->Reset();
+        player1->Reset();
+        player2->Reset();
+    }
 };
 int main()
 {
-    cBall c(0, 0);
-    cout << c << endl;
-    c.randomDirection();
-    cout << c << endl;
-    c.Move();
-    cout << c << endl;
-    c.randomDirection();
-    c.Move();
-    cout << c << endl;
-    c.randomDirection();
-    c.Move();
-    cout << c << endl;
+    
 
     return 0;
 }
