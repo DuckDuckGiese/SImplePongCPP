3c0226e4b88b0564ccda9e841da4628f40e79851
Ricky Giese <richard.giese@icloud.com>
2023-10-27 22:45:27 -0500

    paddles

--- main.cpp
+++ main.cpp
@@ -65,7 +65,29 @@ public:
         return o;
     }
 };
-
+class cPaddle
+{
+private:
+    int x, y;
+    int originalX, originalY;
+public:
+    cPaddle()
+    {
+        x = y = 0;
+    }
+    cPaddle(int posX, int posY) : cPaddle()
+    {
+        originalX = posX;
+        originalY = posY;
+        x = posX;
+        y = posY;
+    }
+    inline void Reset() { x = originalX; y = originalY; }
+    inline int getX() { return x; }
+    inline int getY() { return y; }
+    inline void moveUp() { y--; }
+    inline void moveDown() { y++; }
+};
 int main()
 {
     cBall c(0, 0);
